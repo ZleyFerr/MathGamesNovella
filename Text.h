3ef4db1e1a1a9f2b5369a921dcfb7a1ceb4a88d7
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2024-10-30 21:03:20 +0300

    Update Text.h

--- Text.h
+++ Text.h
@@ -11,17 +11,19 @@ void text_background()
 {
     text_background_texture.loadFromFile("Images/text_background.png");
     text_background_sprite.setTexture(text_background_texture);
-    text_background_sprite.setScale(0.4, 0.4);
-    text_background_sprite.setPosition(1300, 400);
+    text_background_sprite.setScale(1.5, 0.7);
+    text_background_sprite.setPosition(30, 720);
+    text_background_sprite.setColor(sf::Color::Black);
+    text_background_sprite.setColor(sf::Color(0,0,0, 128));
 }
 
 void novella_scene_text() 
 {
     font.loadFromFile("Fonts/MOSCOW2024.otf");
     novella_text.setFont(font);
-    novella_text.setCharacterSize(35);
+    novella_text.setCharacterSize(24);
     novella_text.setFillColor(sf::Color::White);
     novella_text.setScale(1, 1.1);
-    novella_text.setPosition(sf::Vector2f(250, 650));
-    novella_text.setString(L"приветствую тебя в своей игре!\nМеня зовут Максим\nи я - бог этой игры!");
+    novella_text.setPosition(sf::Vector2f(70, 750));
+    novella_text.setString(L"Ой, привет, привет! Давно не виделись!\nесли вдруг ты забыл, как меня зовут - то я ????????\nмихаил скинь текст заебал!");
 }
