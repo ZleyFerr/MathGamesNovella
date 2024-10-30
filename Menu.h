5aaca9e28360c43551232d1b86d142cc8a5c7b1b
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2024-10-30 19:48:39 +0300

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,34 +1,32 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-//стадия игры и счет
+//СЃС‚Р°РґРёСЏ РёРіСЂС‹ Рё СЃС‡РµС‚
 int game_stage = 0;
 int max_game_stage = 0;
 int score = 0;
 bool loading_check = 1;
 
-//загрузка текстура
 sf::Texture loading_Texture;
 sf::Sprite loading_sprite;
 
-//кнопка закрытия игры
 sf::Texture game_close_texture;
 sf::Sprite game_close_sprite;
 
-void game_close(sf::RenderWindow& window) //закрытие игры
+void game_close(sf::RenderWindow& window) //Р·Р°РєСЂС‹С‚РёРµ РёРіСЂС‹
 {
-    //кнопка закрытия игры
+    //РєРЅРѕРїРєР° Р·Р°РєСЂС‹С‚РёСЏ РёРіСЂС‹
     game_close_texture.loadFromFile("Images/close.png");
     game_close_sprite.setTexture(game_close_texture);
     game_close_sprite.setPosition(925, 800);
     game_close_sprite.setScale(0.2, 0.2);
-    //открытие меню
+    //РѕС‚РєСЂС‹С‚РёРµ РјРµРЅСЋ
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
     {
         game_stage = -1;
         loading_check = 1;
     }
-    //закрытие игры при нажатии на кнопку закрытия
+    //Р·Р°РєСЂС‹С‚РёРµ РёРіСЂС‹ РїСЂРё РЅР°Р¶Р°С‚РёРё РЅР° РєРЅРѕРїРєСѓ Р·Р°РєСЂС‹С‚РёСЏ
     if (game_stage == -1)
     {
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
@@ -42,7 +40,7 @@ void game_close(sf::RenderWindow& window) //закрытие игры
     }
 }
 
-void loading() //экран загрузки
+void loading() //СЌРєСЂР°РЅ Р·Р°РіСЂСѓР·РєРё
 {
     loading_Texture.loadFromFile("Images/loading.jpg");
     loading_sprite.setTexture(loading_Texture);
