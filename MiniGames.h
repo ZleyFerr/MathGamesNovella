d07e5375d2ed3ea716105c4fa810aa6898b42a17
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2024-10-30 19:49:07 +0300

    Update MiniGames.h

--- MiniGames.h
+++ MiniGames.h
@@ -4,14 +4,14 @@
 #include "TicTacToe.h"
 #include <SFML/Graphics.hpp>
 
-//мини игры по умолчанию не запущены
+//РјРёРЅРё РёРіСЂС‹ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ РЅРµ Р·Р°РїСѓС‰РµРЅС‹
 bool tic_tac_minigame_start = 0;
 bool nim_minigame_start = 0;
 bool russian_roullete_minigame_start = 0;
 
-void minigames() //запуск мини игр
+void minigames() //Р·Р°РїСѓСЃРє РјРёРЅРё РёРіСЂ
 {
-    if (tic_tac_minigame_start == 1) tic_tac_toe_main();
+    if (tic_tac_minigame_start == 1) tic_tac_toe_main(); 
     if (nim_minigame_start == 1) nim_main();
     if (russian_roullete_minigame_start == 1) russian_roullete_main();
 }
