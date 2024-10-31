47eb9ae99a6daf0d3f1b8145117325211b613544
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2024-10-31 18:16:54 +0300

    Update MiniGames.h

--- MiniGames.h
+++ MiniGames.h
@@ -11,6 +11,12 @@ bool russian_roullete_minigame_start = 0;
 
 void minigames() //запуск мини игр
 {
+    if (game_stage == 8)
+    {
+         tic_tac_minigame_start = 1;
+         nim_minigame_start = 0;
+         russian_roullete_minigame_start = 0;
+    }
     if (tic_tac_minigame_start == 1) tic_tac_toe_main(); 
     if (nim_minigame_start == 1) nim_main();
     if (russian_roullete_minigame_start == 1) russian_roullete_main();
