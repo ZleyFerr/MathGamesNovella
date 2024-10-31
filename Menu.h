3557b871aecc1fa44343a518d7ce7a6ba3d45d4a
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2024-10-31 11:49:58 +0300

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,25 +1,19 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-
-//стадия игры и счет
+#include "Buttons.h"
+//стадия игры
 int game_stage = 0;
 int max_game_stage = 0;
+
 int score = 0;
+
 bool loading_check = 1;
 
 sf::Texture loading_Texture;
 sf::Sprite loading_sprite;
 
-sf::Texture game_close_texture;
-sf::Sprite game_close_sprite;
-
 void game_close(sf::RenderWindow& window) //закрытие игры
 {
-    //кнопка закрытия игры
-    game_close_texture.loadFromFile("Images/close.png");
-    game_close_sprite.setTexture(game_close_texture);
-    game_close_sprite.setPosition(925, 800);
-    game_close_sprite.setScale(0.2, 0.2);
     //открытие меню
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
     {
@@ -45,4 +39,18 @@ void loading() //экран загрузки
     loading_Texture.loadFromFile("Images/loading.jpg");
     loading_sprite.setTexture(loading_Texture);
     loading_sprite.setScale(1, 1);
+}
+
+void game_continue(sf::RenderWindow& window)
+{
+    if (game_stage == -1)
+    {
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            if (continue_button_sprite.getGlobalBounds().contains(sf::Mouse::getPosition().x, sf::Mouse::getPosition().y))
+            {
+                game_stage = max_game_stage;
+            }
+        }
+    }
 }
