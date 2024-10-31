b72ad172a282311d0b54db7d400080c7bf58c9f9
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2024-10-31 11:49:31 +0300

    Add files via upload

---
+++ Buttons.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+sf::Texture next_button_texture;
+sf::Sprite next_button_sprite;
+
+sf::Texture continue_button_texture;
+sf::Sprite continue_button_sprite;
+
+sf::Texture game_close_texture;
+sf::Sprite game_close_sprite;
+
+void next_button()
+{
+    next_button_texture.loadFromFile("Images/button_next.png");
+    next_button_sprite.setTexture(next_button_texture);
+    next_button_sprite.setScale(0.5, 0.32);
+    next_button_sprite.setPosition(710, 890);
+    next_button_sprite.setColor(sf::Color(136, 0, 0, 250));
+}
+
+void continue_button()
+{
+    continue_button_texture.loadFromFile("Images/button_continue.png");
+    continue_button_sprite.setTexture(continue_button_texture);
+    continue_button_sprite.setScale(0.145, 0.145);
+    continue_button_sprite.setPosition(1025, 800);
+}
+
+void game_close_button()
+{
+    game_close_texture.loadFromFile("Images/close.png");
+    game_close_sprite.setTexture(game_close_texture);
+    game_close_sprite.setPosition(825, 800);
+    game_close_sprite.setScale(0.2, 0.2);
+}
