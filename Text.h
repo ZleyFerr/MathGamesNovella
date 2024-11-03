23d826cb5b5009f70d8e1eb4217c8e12b02a4577
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2024-11-03 22:37:46 +0300

    Update Text.h

--- Text.h
+++ Text.h
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include "Menu.h"
 #include "MiniGames.h"
+#include "Background.h"
 
 sf::Texture text_background_texture;
 sf::Sprite text_background_sprite;
@@ -90,5 +91,74 @@ void novella_scene_text()
         novella_text.setCharacterSize(24);
         novella_text.setPosition(sf::Vector2f(140, 780));
         novella_text.setString(L"Хаха, я же говорила, что лучшая.\nТак и быть, отдраивать будешь лишь парты,\nа не весь класс, иди к Ксюше\nно если еще раз тебя увижу прогуливающим\nто будешь весь класс драить!");
+    }
+    if (game_stage == 10 && nichya > -1)
+    {
+        novella_text.setCharacterSize(24);
+        novella_text.setPosition(sf::Vector2f(110, 810));
+        novella_text.setString(L"Ну привет, меня зовут Даша.\nКсюшу ты обыграл\nно меня ты никогда не победишь!\n");
+    }
+    if (game_stage == 10 && nichya == -1)
+    {
+        novella_text.setCharacterSize(24);
+        novella_text.setPosition(sf::Vector2f(110, 780));
+        novella_text.setString(L"Ну привет, меня зовут Даша.\nКсюше ты проиграл,ха-ха\nно так уж и быть, я дам тебе шанс отыграться)\nНо не надейся, что ты легко победишь!");
+    }
+    if (game_stage == 11)
+    {
+        novella_text.setCharacterSize(35);
+        novella_text.setPosition(sf::Vector2f(125, 750));
+        answer_text.setString(L"-Ним, наверное?\n\n-Догонялки?)");
+        novella_text.setPosition(sf::Vector2f(110, 780));
+        novella_text.setString(L"Как думаешь, во что мы будем играть?");
+    }
+    if (game_stage == 12 && nim_true == 1)
+    {
+        novella_text.setCharacterSize(24);
+        novella_text.setPosition(sf::Vector2f(110, 780));
+        novella_text.setString(L"Да, ты угадал!\nТогда сейчас я объясню тебе правила\nИ кстати, Ним - моя любимая математическая игра!\nпойдем на улицу.");
+    }
+    if (game_stage == 12 && nim_true == 0)
+    {
+        novella_text.setCharacterSize(24);
+        novella_text.setPosition(sf::Vector2f(110, 780));
+        novella_text.setString(L"Нет, ты не угадал!\nИграть мы с тобой, будем в Ним)\nИ кстати, Ним - моя любимая математическая игра!\nСейчас я объясню тебе правила.\nпойдем на улицу.");
+    }
+    if (game_stage == 13)
+    {
+        novella_text.setCharacterSize(24);
+        novella_text.setPosition(sf::Vector2f(110, 780));
+        novella_text.setString(L"Слушай внимательно!\nУ нас есть 4 кучки\nв каждой из них от 2 до 3 монеток\nходим по очереди - ты первый\nбрать из одной кучки можно от 1 до 3 штук\nпобедит тот кто возьмёт последнюю монету");
+    }
+    if (game_stage == 14)
+    {
+        novella_text.setCharacterSize(24);
+        text_background_sprite.setPosition(30, 320);
+        novella_text.setPosition(sf::Vector2f(110, 380));
+        novella_text.setString(L"Мини игра 2 - Ним\nКоличество кучек : 4 \nколичество монет в куче написано под кучей\n1 ход - игрока, 2 - даши\nбрать из одной кучки можно от 1 до 3 штук\nпобедит тот кто возьмёт последнюю монету");
+    }
+
+    if (game_stage == 15 && nim_win == 0)
+    {
+        text_background_sprite.setPosition(30, 720);
+        novella_text.setCharacterSize(24);
+        novella_text.setPosition(sf::Vector2f(110, 780));
+        novella_text.setString(L"Ну, я не секунду не сомневалась в себе)\nно я добрее Ксюши и Каримы\nпоэтому наказание тебе выберет Карина потом.\nона тебя уже заждалась.\nудачи тебе с ней..");
+    }
+
+    if (game_stage == 15 && nim_win == 1)
+    {
+        text_background_sprite.setPosition(30, 720);
+        novella_text.setCharacterSize(24);
+        novella_text.setPosition(sf::Vector2f(110, 780));
+        novella_text.setString(L"Ну, я не секунду не сомневалась в тебе)\nрада, что ты не такой глупый, как думает Карима\nя думаю тебе пора бежать к ней.\nона тебя уже заждалась.\nудачи тебе с ней..");
+    }
+
+    if (game_stage == 16)
+    {
+        text_background_sprite.setPosition(30, 720);
+        novella_text.setCharacterSize(24);
+        novella_text.setPosition(sf::Vector2f(110, 780));
+        novella_text.setString(L"Признаться, я удивлена, думала, ты не дойдешь до меня..\nмне казалось ты уже пойдешь на уроки..\n");
     }
 }
